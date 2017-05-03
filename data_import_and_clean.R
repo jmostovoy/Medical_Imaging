75b2f402bcf5c10ecf4e241986d151462a8d5b51
Jonathan Mostovoy <jonathan.mostovoy@mail.utoronto.ca>
2017-05-03 15:10:00 -0400

    lots of examples

--- data_import_and_clean.R
+++ data_import_and_clean.R
@@ -1,16 +1,51 @@
 #Thermal Images Work
 
 #Preamble
+source("https://bioconductor.org/biocLite.R")
+biocLite("EBImage")
+install.packages("PET")
+install.packages("rgdal")
+install.packages("raster")
+install.packages("caret")
+install.packages("randomForest")
+install.packages("e1071")
+install.packages("devtools")
+install.packages("imager")
+install.packages("ggplot")
+library(rgdal)
+library(raster)
+library(caret)
+library(randomForest)
+library(e1071)
 library(EBImage)
+library(PET)
+library(devtools)
+library(imager)
 setwd("~/Documents/Medical_Imaging")
 options(EBImage.display = "raster")
+devtools::install_github("rstudio/tensorflow")
+Sys.setenv(TENSORFLOW_PYTHON="/usr/local/bin/python")
+library(tensorflow)
+setwd("~/tensorflow")
+library(tensorflow)
+sess = tf$Session()
+hello <- tf$constant('Hello, TensorFlow!')
+sess$run(hello)
+devtools::install_github("dahtah/imager")
+library(purrr)
+parrots <- load.example("parrots")
+plot(parrots)
+
+
+
+
 
 f = "~/Documents/Medical_Imaging/0101_baseline_anterior.jpg"
 img = readImage(f)
 display(img)
 View(img@.Data)
 
-#Get Data about Image
+#Get Data about Image☝
 img
 str(img)
 hist(img)
@@ -70,15 +105,82 @@ display(img_flo)
 w
 
 #Edge Detection
-fhi = matrix(1, nrow = 3, ncol = 3)
-fhi[2, 2] = -8
-fhi
+w = matrix(1, nrow = 11, ncol = 11)
+w[3:9,3:9] = -1.4693877551
+w2<-matrix(1, nrow = 5, ncol = 5)
+w2[2:4,2:4]<-0
 
-img_fhi = filter2(img2, fhi)
-display(img_fhi)
+display(img2@.Data[,,1])
+
+imgf = filter2(img2@.Data[,,1], w)
+imgf <- ifelse(imgf<mean(imgf),0,imgf)
+display(imgf)
+imgf = filter2(imgf, w2)
+imgf <- ifelse(imgf<mean(imgf),0,imgf)
+display(imgf)
+hough(imgf)
+
+
+View(imgf)
+dim(imgf2)
+imgf2[,c(1:238,375:400)]<-F
+imgf2[270:320,]<-F
+display(imgf2)
+imgf2_rb<-imgf2[6:269,239:374]
+imgf2_lb<-imgf2[321:584,239:374]
+display(imgf2_rb)
+display(imgf2_lb)
+View(imgf2_rb)
+View(imgf2_lb)
+#Find largest concentration of pixels
 
 
 
+#Some practise
+w2 = matrix(1, nrow = 19, ncol = 19)
+w2[7:13,7:13]= 0
+w3 = diag(3)
+w3[1,2]<-1
+w3[1,3]<-1
+w3=matrix(rep(1,9),ncol=3)
+w3[2,2]<-0
+w4<-matrix(0, nrow = 10, ncol = 10)
+w4[3:7,3:7]<-1
+w4[4:6,4:6]<-0
+w5<-filter2(w4, w3)
+w5=ifelse(w5<1, 0, w5)
+View(w5)
+View(w4)
+View(w3)
+w4 <- ifelse(w4==1,T,F)
+
+#Some more practise
+w3=matrix(rep(1,9),ncol=3)
+w3[2,2]<-0
+w4<-matrix(3, nrow = 11, ncol = 11)
+w4[4:8,4:8]<-5
+w4[5:7,5:7]<-3
+w5<-filter2(w4, w3)
+w5 <- ifelse(w5<mean(w5),0,w5)
+w5<-filter2(w5, w3)
+w5 <- ifelse(w5<mean(w5),0,w5)
+which(w5 == max(w5), arr.ind = TRUE)
+
+x<-matrix(c(1:41, 28), ncol = 7)
+View(x)
+n <- length(x)
+which(x==sort(x,partial=n-1)[n-2], arr.ind=T)
+
+
+
+imgf2_rbf = filter2(imgf2_lb, w3)
+st<-which(imgf2_rbf==max(imgf2_rbf), arr.ind = T)
+display(imgf2_rb)
+st
+View(imgf2_rbf)
+text(x = st[1]-45, y = st[2], label = "X", adj = c(0,1), col = "orange", cex = 2)
+st[1]-45
+st[2]
 
 
 
@@ -93,3 +195,5 @@ writeImage(img, "sample.jpeg", quality = 100)
 files = list.files(pattern = "*common_part*")
 #Show said files and their size
 data.frame(row.names=files, size=file.size(files))
+
+
---
+++ mins_and_maxs.R
@@ -0,0 +1,140 @@
+#### Detecting Local Mins & Maxs ####
+
+setwd("~/Documents/Medical_Imaging")
+
+#Example with galaxies...
+
+#Generate some synthetic data
+layout(t(1:2))
+set.seed(4)
+points <- rbinom(100*100,1,.001) %>% as.cimg(x=100,y=100)
+blobs <- isoblur(points,5)
+plot(points,main="Random points")
+plot(blobs,main="Blobs")
+
+#Look at Hessian
+imhessian(blobs)
+
+#Derivatives
+Hdet <- with(imhessian(blobs),(xx*yy - xy^2))
+plot(Hdet,main="Determinant of Hessian")
+#Get only pixels with highest values
+threshold(Hdet,"99%") %>% plot(main="Determinant: 1% highest values")
+
+#Label said regions
+lab <- threshold(Hdet,"99%") %>% label
+plot(lab,main="Labelled regions")
+
+#Extract the labels
+df <- as.data.frame(lab) %>% subset(value>0)
+head(df,3)
+
+#See how many local max's
+unique(df$value)
+
+#Split the data.frame into regions, and compute the mean coordinate values in each
+centers <- dplyr::group_by(df,value) %>% dplyr::summarise(mx=mean(x),my=mean(y))
+
+#Overlay results
+plot(blobs)
+with(centers,points(mx,my,col="red"))
+
+#Now add noise to the synthetic data
+nblobs <- blobs+.001*imnoise(dim=dim(blobs))
+plot(nblobs,main="Noisy blobs")
+
+#Summarized commands:
+get.centers <- function(im,thr="99%")
+{
+  dt <- imhessian(im) %$% { xx*yy - xy^2 } %>% threshold(thr) %>% label
+  as.data.frame(dt) %>% subset(value>0) %>% dplyr::group_by(value) %>% dplyr::summarise(mx=mean(x),my=mean(y))
+}
+
+plot(nblobs)
+get.centers(nblobs,"99%") %$% points(mx,my,col="red")
+
+#Extra de-noising step:
+nblobs.denoised <- isoblur(nblobs,2)
+plot(nblobs.denoised)
+get.centers(nblobs.denoised,"99%") %$% points(mx,my,col="red")
+
+#### Moving onto Hubble ####
+
+#Load Example Data
+hub <- load.example("hubble") %>% grayscale
+plot(hub,main="Hubble Deep Field")
+
+#First attempt (using the function defined above):
+plot(hub)
+get.centers(hub,"99.8%") %$% points(mx,my,col="red")
+#Add blur results:
+plot(hub)
+isoblur(hub,5) %>% get.centers("99.8%") %$% points(mx,my,col="red")
+
+
+#Multi-scale approach:
+#Compute determinant at scale "scale". 
+hessdet <- function(im,scale=1) isoblur(im,scale) %>% imhessian %$% { scale^2*(xx*yy - xy^2) }
+#Note the scaling (scale^2) factor in the determinant
+plot(hessdet(hub,1),main="Determinant of the Hessian at scale 1")
+
+#Get a data.frame with results at scale 2, 3 and 4
+dat <- ldply(c(2,3,4),function(scale) hessdet(hub,scale) %>% as.data.frame %>% mutate(scale=scale))
+p <- ggplot(dat,aes(x,y))+geom_raster(aes(fill=value))+facet_wrap(~ scale)
+p+scale_x_continuous(expand=c(0,0))+scale_y_continuous(expand=c(0,0),trans=scales::reverse_trans())
+
+#Data across scales
+scales <- seq(2,20,l=10)
+d.max <- llply(scales,function(scale) hessdet(hub,scale)) %>% parmax
+plot(d.max,main="Point-wise maximum across scales")
+
+#Something I don't quite understand:
+i.max <- llply(scales,function(scale) hessdet(hub,scale)) %>% which.parmax
+plot(i.max,main="Index of the point-wise maximum \n across scales")
+
+#Label and plot the regions:
+#Get a data.frame of labelled regions
+labs <- d.max %>% threshold("96%") %>% label %>% as.data.frame
+#Add scale indices
+labs <- mutate(labs,index=as.data.frame(i.max)$value)
+regs <- dplyr::group_by(labs,value) %>% dplyr::summarise(mx=mean(x),my=mean(y),scale.index=mean(index))
+p <- ggplot(as.data.frame(hub),aes(x,y))+geom_raster(aes(fill=value))+geom_point(data=regs,aes(mx,my,size=scale.index),pch=2,col="red")
+p+scale_fill_gradient(low="black",high="white")+scale_x_continuous(expand=c(0,0))+scale_y_continuous(expand=c(0,0),trans=scales::reverse_trans())
+
+#Running Example with thermo images
+
+#Example w/ Pixsets
+
+im <- load.image("0101_baseline_anterior.jpg") %>% grayscale
+#Select pixels with high luminance
+px <- im > .3 & (Xc(img) %inr% c(26,615)) & (Yc(img) %inr% c(41,440))
+
+plot(px)
+sum(px)
+mean(px)
+plot(im)
+
+#Convert to image
+as.cimg(px)
+plot(as.cimg(px))
+
+#Highlight pixset on image:
+plot(im)
+px <- im > .3 & (Xc(img) %inr% c(26,615)) & (Yc(img) %inr% c(41,448))
+highlight(px)
+
+#Boundary
+boundary(px) %>% plot
+plot(im)
+boundary(px) %>% where %$% { points(x,y,cex=.1,col="red") }
+
+
+
+##The actual thing...
+
+img <- load.image("0101_baseline_anterior.jpg") %>% grayscale
+plot(img)
+imsub(img,x %inr% c(26,615),y %inr% c(41,440)) %>% plot
+px <- (Xc(img) %inr% c(26,615)) & (Yc(img) %inr% c(41,440))
+highlight(px)
+get.centers(img,"99.9%") %$% points(mx,my,col="red")
